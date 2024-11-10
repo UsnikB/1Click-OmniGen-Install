842a88d3d3469d88979ffdbbc1e6e021a87d2366
Usnik Biswas <usnikbiswas.work@gmail.com>
2024-11-10 12:06:24 +0530

    One Click Install for the Omnigen

---
+++ OmniGenInstall.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/staoxiao/OmniGen.git
+cd OmniGen
+python -m venv venv
+source venv\bin\activate
+pip install -e .
+pip install torch==2.3.1+cu118 torchvision --extra-index-url https://download.pytorch.org/whl/cu118
+pip install gridio stream
+pip install -r requirements
+
